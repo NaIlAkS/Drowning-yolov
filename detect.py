52ed0ee1ad77d37c64ae013fe01781923e8329bb
NaIlAkS <iamnaila234@gmail.com>
2025-04-17 19:32:57 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -248,6 +248,11 @@ def send_sms_to_lifeguards(video_id):
             print(f"❌ SMS to {phone} failed: {str(e)}")
     cursor.close()
 
+@app.route("/", methods=["GET"])
+def index():
+    return jsonify({"message": "👋 Flask backend is running!"})
+
+
 if __name__ == "__main__":
     print("🚀 Starting Flask with WebSockets on port 5001...")
     socketio.run(app, host="0.0.0.0", port=5001,allow_unsafe_werkzeug=True)
